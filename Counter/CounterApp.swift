dcbaa4223726a93a04030f08354978ee01d3954b
Kyra Henningson <kyrahenningson@hotmail.com>
2022-11-20 11:28:12 -0800

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Kyra Henningson on 2022-11-20.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
