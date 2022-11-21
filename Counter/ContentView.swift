8d9bb2a30342e0c7d03c9c647f284601d1833938
Kyra Henningson <kyrahenningson@hotmail.com>
2022-11-20 17:22:49 -0800

    counter app swift

--- Counter/ContentView.swift
+++ Counter/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var count = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Counter").foregroundColor(.white).font(.system(size: 48, weight: .bold))
+                    .padding(.top, 48)
+                    .padding(.bottom, 48)
+                Text("\(count)")
+                    .foregroundColor(.white).font(.system(size: 48, weight: .bold))
+                Spacer()
+                HStack(spacing: 48) {
+                    if (count > 0 ) {
+                        Button {
+                            count -= 1
+                        } label: {
+                            Text ("-").foregroundColor(.blue).font(.system(size: 48, weight: .bold))
+                        }
+                    }
+                    Button {
+                        count += 1
+                    } label: {
+                        Text("+").foregroundColor(.blue).font(.system(size: 48, weight: .bold))
+                    }
+                }
+            
+            }
         }
-        .padding()
     }
 }
 
